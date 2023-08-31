ab77b32c0179bf07ca6f4f20028ea9b7184cdf68
Oumaima Nejjari <oumanj1998@gmail.com>
2023-08-31 13:06:57 +0100

    Implementing required changes

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -10,8 +12,8 @@ class Student < Person
     '¯\(ツ)/¯'
   end
 
-  def classroom=(classroom)
+  def classroom(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
